e11682909f80367ff4bcb957149e81e55dcd6cce
kamru <kamrulbt@gmail.com>
2024-12-20 19:37:28 +0600

    before score

--- scripts/alpha-clash.js
+++ scripts/alpha-clash.js
@@ -1,3 +1,20 @@
+// function play(){
+//     // hide the home screen by clicking play now...
+//     const homeSection = document.getElementById('home-screen');
+//     homeSection.classList.add('hidden');
+//     // show the playground by clicking play now.....
+//     const playgroundSection = document.getElementById('play-ground');
+//     playgroundSection.classList.remove('hidden');
+//     console.log(homeSection.classList)
+// }
+function continueGame(){
+    const alphabet = getARandomAlphabet();
+    const currentAlphabetElement = document.getElementById('current-element');
+    currentAlphabetElement.innerText = alphabet;
+    setBackgroundColorById(alphabet);
+}
 function play(){
-    console.log('play start')
+    hideElementById('home-screen');
+    showElementById('play-ground');
+    continueGame();
 }
